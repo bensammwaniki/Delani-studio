1fcfe6dd1c6f4788ef5a1db9b5e1cf8ff70ab915
bensammwaniki <bensammwaniki@gmail.com>
2021-07-26 02:21:18 +0300

    added toggle effect

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+      $("#designinfo").toggle();
+    });
+    $("#dev").click(function(){
+        $("#devinfo").toggle();
+      });
+      $("#prod").click(function(){
+        $("#prodinfo").toggle();
+      });
+
+
+  });
